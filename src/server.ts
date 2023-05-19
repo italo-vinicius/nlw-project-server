414631796cef4a1f60f764e3265ac0289d59cd9d
Italo Vinicius <italoviniciusrope@gmail.com>
2023-05-19 18:41:07 -0300

    wip

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,38 @@
+import "dotenv/config";
+
 import fastify from "fastify";
-import { PrismaClient } from "@prisma/client";
-
+import cors from "@fastify/cors";
+import jwt from "@fastify/jwt";
+import multipart from "@fastify/multipart";
+import { memoriesRoutes } from "./routes/memories";
+import { authRoutes } from "./routes/auth";
+import { uploadRoutes } from "./routes/uploads";
+import { resolve } from "path";
+  
 const app = fastify();
-const prisma = new PrismaClient();
 
-app.get("/", async () => {
-  const users = await prisma.user.findMany();
-  return users;
+app.register(multipart);
+app.register(require("@fastify/static"), {
+  root: resolve(__dirname, "../uploads"),
+  prefix: "/uploads",
 });
+
+app.register(cors, {
+  origin: true,
+});
+
+app.register(jwt, {
+  secret: "spacetime",
+});
+
+app.register(uploadRoutes);
+app.register(authRoutes);
+app.register(memoriesRoutes);
 
 app
   .listen({
     port: 8000,
+    host: "127.0.0.1",
   })
   .then(() => {
     console.log("😎 rodando fino demais chefe");
